2ceb36099c7775d3b09534abf16bd0d32cf62c8a
Isaac2040 <scgisore@gmail.com>
2024-07-08 11:12:43 +0300

    Inverted pyramid created successfully

--- main.c
+++ main.c
@@ -2,6 +2,24 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    //create an inverted pyrmaid
+
+    int n=20;
+    for (int i =1;i<=n;i++)
+    {
+        //loop to create spaces
+        for (int j=1;j<(2*i-i);j++)
+        {
+            printf(" ");
+        }
+
+        //a loop to print the * after the spaces.
+
+        for(int j=1; j<(2*n-2*i);j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
