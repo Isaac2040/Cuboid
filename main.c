eea900addb9d683658f2cffa06651787f07012df
Isaac2040 <scgisore@gmail.com>
2024-07-08 10:37:47 +0300

    Create pyramid.
    Pyramid created successfully

--- main.c
+++ main.c
@@ -2,7 +2,23 @@
 
 int main(void)
 {
-    int n=20; //defines the number of rows
+    const int n=20; //defines the number of rows
+    for (int i=1;i<=n;i++)
+    {
+        //form a function to print empty spaces
+        for(int j=1;j<=(n-i);j++)
+        {
+            printf(" ");
 
+        }
+
+        //form another function to print *
+
+        for(int j=1; j<=(2*i-1);j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
